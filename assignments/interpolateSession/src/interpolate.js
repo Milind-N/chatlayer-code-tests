aa752ffebd3cb2d8d42f9c73d67f3ec163369e1b
Milind Nayse <milindnayse@gmail.com>
2020-09-24 07:35:39 +0200

    Add interpolate logic with regex

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,5 +1,13 @@
+const isEmpty = (sessionArray) => {
+  return sessionArray.length === 0
+}
+
 const interpolate = (value, session = {}, options = {}) => {
-    //TODO
+  const { leftDelimiter, rightDelimiter} = options;
+  const sessionArray = Object.keys(session)
+  const list = isEmpty(sessionArray) ? '.*?' : sessionArray.join('|');
+  var regExp = new RegExp( leftDelimiter + '(' + list + ')' + rightDelimiter , 'g');
+  return value.replace(regExp, function (match, capture) { return session[capture] ? session[capture] : ''  });
 };
 
 module.exports = { interpolate }
